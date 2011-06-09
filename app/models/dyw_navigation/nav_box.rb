429caf0f94ad393e658220bac1eeea9b8269feb1
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-06-09 14:46:17 +0200

    attr_accessible in model uitgebreid

--- app/models/dyw_navigation/nav_box.rb
+++ app/models/dyw_navigation/nav_box.rb
@@ -1,6 +1,8 @@
 module DywNavigation
   class NavBox < ActiveRecord::Base
-		attr_accessible :image
+		attr_accessible :image, :title, :link
+		validates_presence_of :title, :link
+		validates_uniqueness_of :title, :link
 		mount_uploader :image, ImageUploader
   end
 end
