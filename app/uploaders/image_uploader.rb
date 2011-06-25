5853a55d4a1d9ab084d6a656b3a01c6841083000
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-06-25 12:08:39 +0200

    Thumb kleiner gemaakt

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,7 +35,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process :resize_to_limit => [250, 250]
+     process :resize_to_limit => [250, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
